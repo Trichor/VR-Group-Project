f8fbbec09e0b98d919d5bc0e594891fcae14ed25
BentleyGoh <bentleygoh@gmail.com>
2024-05-24 09:39:41 +0800

    Update BaseballSound
    Code Updated to remove collision on startup

--- Assets/Scripts/Object Scripts/BaseballSound.cs
+++ Assets/Scripts/Object Scripts/BaseballSound.cs
@@ -5,11 +5,30 @@ using UnityEngine;
 public class BaseballSound : MonoBehaviour
 {
     public AudioSource audioPlayer;
-    public AudioClip ballBounce;
+    public AudioClip bounceSound;
+
+    // Minimum velocity to trigger sound collision
+    public float minCollisionVelocity = 0.5f;
+    private bool canPlaySound = false;
+
+    void Start()
+    {
+        // Delay sound activation for 0.5s
+        StartCoroutine(EnableSoundAfterDelay(0.5f));
+    }
+
+    IEnumerator EnableSoundAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        canPlaySound = true;
+    }
 
     void OnCollisionEnter(Collision collision)
     {
-        // Adjusting volume scale
-        audioPlayer.PlayOneShot(ballBounce, 0.7f);
+        if (canPlaySound && collision.relativeVelocity.magnitude > minCollisionVelocity)
+        {
+            // Adjust volume scale
+            audioPlayer.PlayOneShot(bounceSound, 0.7f);
+        }
     }
 }
